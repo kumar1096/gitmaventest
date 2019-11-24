477f6900e797d8ae3ae8a0666be1b7f6d058e4ab
praveen <praveen51.rymec@gmail.com>
2019-11-24 18:55:42 +0530

    gitmaventest

---
+++ maventest11/src/test/java/kumar11/selectingtest.java
@@ -0,0 +1,27 @@
+package kumar11;
+
+import org.testng.annotations.Test;
+
+public class selectingtest {
+	@Test
+	public void methodone()
+	{
+		System.out.println("welcome to methodone");
+	}
+	@Test
+	public void methodtwo()
+	{
+		System.out.println("welcome to methodtwo");
+	}
+	@Test
+	public void methodthree()
+	{
+		System.out.println("welcome to methodthree");
+	}
+	@Test
+	public void methodfour()
+	{
+		System.out.println("welcome to methodfour");
+	}
+}
+
